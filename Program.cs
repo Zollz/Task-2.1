5e39a9e602c0d0db3664695efc3be3f8bf78942a
Zollz <eddie_sn@Hotmail.com>
2017-07-13 16:04:20 +1000

    update#3

--- Program.cs
+++ Program.cs
@@ -13,11 +13,21 @@ public class Program
 
     Console.Write("Enter Your Name: ");
     name = Console.ReadLine();
-    Console.Write("Hey + name");
+    Console.Write("Hey " + name);
 
-    Console.Write("What is your height?");
-    string inputText = Console.ReadLine();
-    inputText = Convert.ToInt32(heightInCM);
+    Console.Write("Enter Your Height in Centimetres: ");
+    inputText = Console.ReadLine();
+    heightInCM = Convert.ToInt32(inputText);
+    heightInMeters = heightInCM / 100.0;
+    Console.Write("Your Height In Metres Is: " + heightInMeters);
+
+    Console.Write("Enter Your Weight in Kilograms: ");
+    inputText = Console.ReadLine();
+    weightInKG = Convert.ToDouble(inputText);
+    bmi = weightInKG / (heightInMeters * heightInMeters);
+    Console.Write("Your BMI Is: " + bmi);
+
+    
     
 
 
