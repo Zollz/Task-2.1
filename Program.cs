2863d141fd0ae84c03528db03c6ac06de5be9411
Zollz <eddie_sn@Hotmail.com>
2017-07-13 13:11:38 +1000

    update#2

--- Program.cs
+++ Program.cs
@@ -2,9 +2,24 @@ using System;
 using SplashKitSDK;
 
 public class Program
+
+
 {
     public static void Main()
     {
+    string name, inputText;
+    int heightInCM;
+    double weightInKG, heightInMeters, bmi;
+
+    Console.Write("Enter Your Name: ");
+    name = Console.ReadLine();
+    Console.Write("Hey + name");
+
+    Console.Write("What is your height?");
+    string inputText = Console.ReadLine();
+    inputText = Convert.ToInt32(heightInCM);
+    
+
 
     }
 }
